29e6bf8a779cbedc038f7fd52f4eb8e8e0920688
Steve Chan <sychan168@gmail.com>
2017-10-31 13:46:16 -0700

    Fixed bad conditional

--- dockerize.sh
+++ dockerize.sh
@@ -3,12 +3,16 @@
 # Script that wraps the the dockerize command, and then sets up a netcat listener
 # on port 8080 to mark that it has completed. This is a hack to deal with race conditions
 # between a container that initializes a database, and the apps that require the database
-# to be initialized already. Apps the depend on the initialization step can check for
+# to be initialized already. Apps that depend on the initialization step can check for
 # a listener on port 8080 from this image as a signal that the database has completed
 # initialization
 #
 # sychan@lbl.gov
 
 /kb/deployment/bin/dockerize $@
-echo "Starting listener on port 8080 to signal that initialization is complete"
-nc -lk 8080
+exit_status=$?
+if [ $exit_status -eq 0 ]; then
+    echo "Starting listener on port 8080 to signal that initialization is complete"
+    nc -lk 8080
+fi
+exit $exit_status
